d7de8f7f2d6c258e2f2a3b3e710d158ba7874465
AttilaART <amwpinter@icloud.com>
2025-05-02 14:10:28 +0200

    Started work on new windows based design

--- WiSim/frontend/src (old)/store.ts
+++
@@ -1,9 +0,0 @@
-import { writable } from 'svelte/store';
-import { simulation } from '../wailsjs/go/models';
-
-const month_counter = writable(1)
-const company = writable(0)
-const loading = writable(false)
-const decisions = writable(new simulation.Decisions)
-
-export { month_counter, loading, company, decisions }
