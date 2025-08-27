b5fb8a5b0f67d67ae085e9285815f0ee65ebaf17
AttilaART <amwpinter@icloud.com>
2025-08-27 23:33:57 +0200

    add missing files

---
+++ wisim-nouveau/src/components/reasearch.svelte
@@ -0,0 +1,41 @@
+<script>
+	/** @type {{clientState: import("$lib/javascript/simulation").clientState, updateDecisions: (decisions: import("$lib/javascript/simulation").Decisions)=>void}} */
+	let { clientState = $bindable(), updateDecisions } = $props();
+</script>
+
+<h2>Research</h2>
+<form
+	onchange={() => {
+		updateDecisions(clientState.decisions);
+	}}
+>
+	<label for="">
+		Quality Research
+		<input
+			type="number"
+			step="10000"
+			bind:value={clientState.decisions.Research.Quality}
+			max="1000000000"
+		/>
+	</label>
+
+	<label for="">
+		Ecology Research
+		<input type="number" step="10000" bind:value={clientState.decisions.Research.Ecology} />
+	</label>
+
+	<label for="">
+		Production Speed Research
+		<input type="number" step="10000" bind:value={clientState.decisions.Research.Production_cost} />
+	</label>
+
+	<label for="">
+		Durability Research
+		<input type="number" step="10000" bind:value={clientState.decisions.Research.Durability} />
+	</label>
+
+	<label for="">
+		Marketing Research
+		<input type="number" step="10000" bind:value={clientState.decisions.Research.Promotion} />
+	</label>
+</form>
